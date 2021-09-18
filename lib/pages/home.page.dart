de9fab7110674b91bd24b3e2acc0d6f1be5569b2
Diego Alejandro Asencio Cuellar <diegoasencio96@gmail.com>
2021-09-17 19:17:17 -0500

    base project

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inicio"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Text("Texto de prueba");
+  }
+}
